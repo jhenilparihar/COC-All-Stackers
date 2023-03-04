96d9fd52b79fd6b4b0bba5e77890cf1ccc689154
Harsh Ghosalkar <ghosalkarharsh454@gmail.com>
2023-03-04 17:16:36 +0530

    added browser router

---
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,24 @@
+import { useState, Component } from "react";
+
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     
+    };
+  }
+
+ 
+
+  render() {
+    return (
+      <>
+      <button>get Token</button>
+      <button>Buy Token</button>
+      <button>Create Poll</button>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
