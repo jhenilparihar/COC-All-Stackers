9211de428195bc8a4bc22fdf5791ce73901ca29e
Jhenil Parihar <jhenilparihar@gmail.com>
2023-03-05 09:24:29 +0530

    commit

--- client/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-import abi from './Evoting.json';
-
-export const contractABI = abi.abi;
-export const contractAddress = '0xD35Ab99b8dCd7d1feB44a3e593d3b12B96779037';
-
-// 0x4278C8005Adef0ED5e51f5Ff4dFA2610784E9554
